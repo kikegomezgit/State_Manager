909ee15eb3ad78584ec5f29441e9deeabf151c28
lugomez <lugomez@hebmex.com>
2024-11-29 16:43:53 -0600

    work in progress

--- database.js
+++ database.js
@@ -1,26 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-//order schema
-// const order = new Schema({
-//     order_id: String,
-//     workflow: String,
-//     items: Array,
-//     desired_date: Date,
-//     status: { type: String, default: 'pending' },
-//     version: { type: Number, default: 0 }  // Version field to track changes
-// });
-//process-steps 
-//run once per day, get processStepsConfiguration.find(workflow,action).order by numerical_order  steps: [{}]
-
 //Step
 const stepSchema = new Schema({
     step_name: { type: String, required: true },
     numerical_order: { type: Number, required: true },
-    // workflow: { type: String, required: true },
     action: { type: String, required: true },//brief description of action
     api_call_id: { type: Schema.Types.ObjectId, ref: 'ApiCall' },
     status: { type: String, enum: ['pending', 'in progress', 'completed', 'failed', null], default: null },
     response: { type: Schema.Types.Mixed, default: null },
+    active: { type: Boolean, required: true },
     error: { type: Schema.Types.Mixed, default: null },
     end_time: { type: Date, default: null },
 });
@@ -28,7 +16,7 @@ const stepSchema = new Schema({
 const processStepsConfiguration = new Schema({
     name: String,
     workflow: String,
-    // action: String,
+    queue: { type: String, required: true },
     steps: [stepSchema]
 });
 
@@ -51,7 +39,7 @@ const apiCallSchema = new Schema({
     workflow: { type: String, required: false },
     url: { type: String, required: true },
     method: { type: String, enum: ['GET', 'POST', 'PUT', 'DELETE'], required: true },
-    request_attributes: { type: Schema.Types.Mixed, default: null },//{body:{slot:"78"},headers:{}} authorization will reuse this schema but specified
+    request_attributes: { type: Schema.Types.Mixed, default: null },
     response_attributes: { type: Schema.Types.Mixed, default: null },
     timestamp: { type: Date, default: Date.now },
 });
